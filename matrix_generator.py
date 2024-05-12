d0d3d81925890adf86aa85463c46dbae37bc9b4e
gitworkflows <118260833+gitworkflows@users.noreply.github.com>
2024-05-13 03:05:53 +0600

    Create matrix_generator.py

---
+++ matrix_generator.py
@@ -0,0 +1,71 @@
+import json
+from glob import glob
+from os.path import exists
+
+import yaml
+from git import Repo
+
+matrix = {"include": []}
+excluded_files = [  # Changes to those files shouldn't trigger a build
+    '.gitignore',
+    'CHANGELOG.md',
+    'README.md',
+    '.github/dependabot.yml',
+]
+
+
+def get_diff_files_list():
+    repo = Repo('.')
+    modified_files = repo.commit("origin/master").diff(repo.commit())
+    changedFiles = [item.a_path for item in modified_files]
+    return changedFiles
+
+
+def filter_excluded_files(changedFiles):
+    filteredFiles = [
+        file for file in changedFiles if file not in excluded_files
+    ]
+    return filteredFiles
+
+
+def get_paths(changedFiles, unfilteredFiles):
+    paths = []
+    if changedFiles == []:
+        if unfilteredFiles == []:
+            # Master or tag job with no diff, builds everything
+            return glob("*/")
+        else:
+            # All files were previously excluded, builds nothing
+            return []
+    for file in changedFiles:
+        if "/" not in file or "/src" in file or ".github" in file:
+            return glob("*/")
+        else:
+            split_path = file.split("/")
+            paths.append(split_path[0])
+    return set(paths)
+
+
+def generate_matrix(paths):
+    for image_folder in paths:
+        with open("base_config.yml", 'r') as base_config:
+            if exists("{}/config.yml".format(image_folder)):
+                with open("{}/config.yml".format(image_folder), 'r') as config:
+                    full_config = base_config.read() + config.read()
+
+                    image_config = yaml.safe_load(full_config)
+
+                    for version in image_config["versions"]:
+                        matrix["include"].append(
+                            {
+                                "image": image_folder.replace("/", ""),
+                                "version": str(version)
+                            }
+                        )
+    print(json.dumps(matrix))
+
+
+changedFiles = get_diff_files_list()
+filteredFiles = filter_excluded_files(changedFiles)
+paths = get_paths(filteredFiles, changedFiles)
+generate_matrix(paths)
